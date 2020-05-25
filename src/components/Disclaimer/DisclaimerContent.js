acf163f7275e8143e34a28852f40fe94afd2da9b
theuniquecreations <54184830+theuniquecreations@users.noreply.github.com>
2020-05-25 13:45:48 +0530

    -- sathya

--- src/components/Disclaimer/DisclaimerContent.js
+++ src/components/Disclaimer/DisclaimerContent.js
@@ -11,25 +11,25 @@ const DisclaimerContent = () => {
         solicit work or advertise for our services. The user acknowledges the
         following:
       </p>
-
-      <p>
+<ul>
+      <li>
           There has been no advertisement, personal communication, solicitation,
           invitation or inducement of any kind whatsoever from us or any of our
           members to solicit any work through this website;
-        </p>
+        </li>
 
-        <p>
+        <li>
           The user wishes to gain more information about us for his/her own
           information and use
-        </p>
-        <p>
+        </li>
+        <li>
           The information about us is provided to the user only on his/her
           specific request and any information obtained or materials downloaded
           from this website is completely at the user’s volition and any
           transmission, receipt or use of this site would not create any
           lawyer-client relationship.
-        </p>
-    
+        </li>
+        </ul>
       <i>
         The information provided under this website is solely available at your
         request for informational purposes only, should not be interpreted as
