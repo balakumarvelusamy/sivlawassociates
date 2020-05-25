ac490f6f3024959786f82d255200c146a85a3127
Balakumar <theuniquecreations2017@gmail.com>
2020-05-25 13:10:54 -0400

    Bala - footer logo fix

--- src/components/ContactArea/ContactContent.js
+++ src/components/ContactArea/ContactContent.js
@@ -4,15 +4,9 @@ import React from 'react';
 const ContactContent = () => {
     return (
         <div className="contactInfo">
-            <span>For Our Honorabe Clients</span>
-            <h2>Free Consulting</h2>
-            <h6>Call us 24/7 at (888)123-4567 or fill out the form.</h6>
-            <p>
-                It is a long established fact that a reader will be distracted
-                by the readable content of a page when looking at its layout.
-                The point of using Lorem Ipsum is that it has a more-or-less
-                able English.
-        </p>
+            <span>For Our Honorabe Clients</span><br/>
+            <h2>Contact Us</h2>
+            
         </div>
     )
 }
--- src/components/FooterArea/index.js
+++ src/components/FooterArea/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../../images/logo/logo.png'
+import logo from '../../images/logo/slalogo.png'
 import './style.scss'
 const footerLinks = [
     {
@@ -12,7 +12,7 @@ const footerLinks = [
         ]
     },
     {
-        title: 'Area of Practice+', menus: [
+        title: 'Area of Practice', menus: [
             { name: 'Alternate Dispute Resolution', route: 'alt-dispute-resolution' },
             { name: 'Banking & Finance', route: 'banking-finance' },
             { name: 'Labour Law', route: 'labourlaw' },
@@ -47,7 +47,9 @@ const FooterArea = () => {
                                 <Link to="/">
                                     <img src={logo} alt="" />
                                 </Link>
-                                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature</p>
+                                <p>Siva Law Associates, a law firm established in 2009, our firm consists of well experienced attorney’s dealing with variety of cases mainly civil, criminal, Bank cases & Corporate matters on behalf of clients, companies and banks.
+
+</p>
                             </div>
                         </div>
                         {footerLinks.map((menu, i) => (
@@ -70,13 +72,13 @@ const FooterArea = () => {
                     <div className="footerBottomContent">
                         <div className="row">
                             <div className="col-md-8 col-sm-10 col-12">
-                                <span>Privacy Policy | © 2020 Developed by TheUniqueCreations</span>
+                                <span>Privacy Policy | © 2020 Developed by <a href="www.instagram.com/theuniquecreations">TheUniqueCreations</a></span>
                             </div>
                             <div className="col-md-4 col-sm-2 col-12">
                                 <ul className="socialListFooter">
-                                    <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                    {/* <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li> */}
+                                    <li><a href="www.instagram.com/theuniquecreations"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
                                 </ul>
                             </div>
                         </div>
