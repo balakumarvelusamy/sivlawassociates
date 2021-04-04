3cb614db35718b5241fb852a6ca66da044d1861b
“theuniquecreations” <theuniquecreations2017@gmail.com>
2021-04-04 07:28:25 -0400

    disclaimer fix

--- src/components/Disclaimer/DisclaimerContent.js
+++ src/components/Disclaimer/DisclaimerContent.js
@@ -4,36 +4,34 @@ import "./style.scss";
 
 const DisclaimerContent = () => {
   return (
-    <div >
-        <p className="sm">
-     <h6><b>Disclaimer </b>( Siva Law Associates)</h6>
-    
+    <div className="discalimercontent">
+      <p className="smsm">
+        <h6>
+          <b>Disclaimer </b>( Siva Law Associates)
+        </h6>
         As per the rules of the Bar Council of India, we are not permitted to
         solicit work or advertise for our services. The user acknowledges the
-        following:
-         There has been no advertisement, personal communication, solicitation,
-          invitation or inducement of any kind whatsoever from us or any of our
-          members to solicit any work through this website;
-     
-          The user wishes to gain more information about us for his/her own
-          information and use
-    
-          The information about us is provided to the user only on his/her
-          specific request and any information obtained or materials downloaded
-          from this website is completely at the user’s volition and any
-          transmission, receipt or use of this site would not create any
-          lawyer-client relationship.
-       </p>
-       <p>
-      <i>
-        The information provided under this website is solely available at your
-        request for informational purposes only, should not be interpreted as
-        soliciting or advisement nor does such information constitute legal
-        advice. We are not liable for any consequence of any action taken by the
-        user relying on material/information provided on this website. In cases
-        where the user has any legal issues, he/she in all cases must seek
-        independent legal advice.
-      </i></p>
+        following: There has been no advertisement, personal communication,
+        solicitation, invitation or inducement of any kind whatsoever from us or
+        any of our members to solicit any work through this website; The user
+        wishes to gain more information about us for his/her own information and
+        use The information about us is provided to the user only on his/her
+        specific request and any information obtained or materials downloaded
+        from this website is completely at the user’s volition and any
+        transmission, receipt or use of this site would not create any
+        lawyer-client relationship.
+      </p>
+      <p className="smsm">
+        <i>
+          The information provided under this website is solely available at
+          your request for informational purposes only, should not be
+          interpreted as soliciting or advisement nor does such information
+          constitute legal advice. We are not liable for any consequence of any
+          action taken by the user relying on material/information provided on
+          this website. In cases where the user has any legal issues, he/she in
+          all cases must seek independent legal advice.
+        </i>
+      </p>
     </div>
   );
 };
--- src/components/Disclaimer/index.js
+++ src/components/Disclaimer/index.js
@@ -1,28 +1,37 @@
-import React from 'react'
-import SectionTitle from '../Title'
-import './style.scss'
+import React from "react";
+import SectionTitle from "../Title";
+import "./style.scss";
 
 // images
-import portfolio1 from '../../images/studies/1.jpg'
-import portfolio2 from '../../images/studies/2.jpg'
-import portfolio3 from '../../images/studies/3.jpg'
-import portfolio4 from '../../images/studies/4.jpg'
-import portfolio5 from '../../images/studies/5.jpg'
-import DisclaimerContent from '../Disclaimer/DisclaimerContent'
+import portfolio1 from "../../images/studies/1.jpg";
+import portfolio2 from "../../images/studies/2.jpg";
+import portfolio3 from "../../images/studies/3.jpg";
+import portfolio4 from "../../images/studies/4.jpg";
+import portfolio5 from "../../images/studies/5.jpg";
+import DisclaimerContent from "../Disclaimer/DisclaimerContent";
 
 const portfolioItem = [
-    { images: portfolio1, title: 'General Service', subtitle: 'Corporate' },
-    { images: portfolio2, title: 'Personal Issue', subtitle: 'General' },
-    { images: portfolio3, title: 'Business Accounting', subtitle: 'Business' },
-    { images: portfolio4, title: 'Accounting issue', subtitle: 'Criminal' },
-    { images: portfolio5, title: 'Business Accounting', subtitle: 'Family Issue' }
-]
-const Disclaimer = ({ title, subTitle, className, fullWidth, portfolioItem }) => {
-    return (
-        <>
-        <DisclaimerContent></DisclaimerContent>
-       
-        </>
-    )
-}
-export default Disclaimer
+  { images: portfolio1, title: "General Service", subtitle: "Corporate" },
+  { images: portfolio2, title: "Personal Issue", subtitle: "General" },
+  { images: portfolio3, title: "Business Accounting", subtitle: "Business" },
+  { images: portfolio4, title: "Accounting issue", subtitle: "Criminal" },
+  {
+    images: portfolio5,
+    title: "Business Accounting",
+    subtitle: "Family Issue",
+  },
+];
+const Disclaimer = ({
+  title,
+  subTitle,
+  className,
+  fullWidth,
+  portfolioItem,
+}) => {
+  return (
+    <>
+      <DisclaimerContent className="disclaimercontent"></DisclaimerContent>
+    </>
+  );
+};
+export default Disclaimer;
