c7492bf20ba870df68cec95f6880445b68a53148
theuniquecreations <vbalakumar.cse@gmail.com>
2021-12-21 15:37:19 -0500

    fix

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -1,58 +1,44 @@
-import React, { useState } from 'react'
-import './style.scss'
+import React, { useState } from "react";
+import "./style.scss";
 import { fetchService } from "../Services/Globalfunction";
-import config from "../../config.json"
+import config from "../../config.json";
 const NewsLetter = ({ className }) => {
-    const [email, setEmail] = useState('')
-   
-    const submitHandler = async event =>{
-        event.preventDefault()
-        console.log('email =',email)
+  const [email, setEmail] = useState("");
 
-        const msg = {
-            to: config.owneremail,
-            from: config.fromemail,
-            fromname:"Siva Law Associates",
-            toname: "Siva Law Associates",
-            subject: "Newsletter subscription - "+email,
-            body:   'Subscription Email : '+ email ,
-            bodyhtml:
-            '<p> Subscription Email : '+ email + '</p><br/>'
-           
-          };
-        const response = await fetchService(
-            "Email",
-            "SendEmail",
-            "POST",
-            msg
-          );
-          if (response===200){
-        
-          console.log("success");
-  
-          }
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    console.log("email =", email);
+
+    const msg = {
+      to: config.owneremail,
+      from: config.fromemail,
+      fromname: "Siva Law Associates",
+      toname: "Siva Law Associates",
+      subject: "Newsletter subscription - " + email,
+      body: "Subscription Email : " + email,
+      bodyhtml: "<p> Subscription Email : " + email + "</p><br/>",
+    };
+    const response = await fetchService("Email", "SendEmail", "POST", msg);
+    if (response === 200) {
+      console.log("success");
     }
-    return (
-        <div className={className}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="newsletterWrap">
-                            <h3>Subscribe  Newsletter</h3>
-                            <form onSubmit={submitHandler} className="newsletterForm">
-                                <input
-                                    type="email"
-                                    value={email}
-                                    onChange={e => setEmail(e.target.value)}
-                                    className="form-control"
-                                    placeholder="Email Address" />
-                                <button type="submit">subscribe</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  };
+  return (
+    <div className={className}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="newsletterWrap">
+              <h3>Subscribe Newsletter</h3>
+              <form onSubmit={submitHandler} className="newsletterForm">
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="Email Address" />
+                <button type="submit">subscribe</button>
+              </form>
             </div>
+          </div>
         </div>
-    )
-}
-export default NewsLetter
+      </div>
+    </div>
+  );
+};
+export default NewsLetter;
